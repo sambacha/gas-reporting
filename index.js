20881ee8676fa30df3f844f98d4641bfe3c17850
sam <sam@freighttrust.com>
2021-02-21 06:25:19 -0800

    feat(init): release

---
+++ index.js
@@ -0,0 +1,32 @@
+
+/**
+* @file Gas Response Fields
+*/
+
+'use strict';
+
+
+module.exports = {
+lastUpdate*	string($date-time)
+title: 'Last update'
+lowest*	string
+title: Lowest
+maxLength: 20
+minLength: 1
+safeLow*	string
+title: 'Safe low'
+maxLength: 20
+minLength: 1
+standard*	string
+title: 'Standard'
+maxLength: 20
+minLength: 1
+fast*	string
+title: Fast
+maxLength: 20
+minLength: 1
+fastest*	string
+title: 'Fastest'
+maxLength: 20
+minLength: 1
+};
