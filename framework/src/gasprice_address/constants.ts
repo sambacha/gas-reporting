ecf2ccc30cb5f2c643157ba188b0a1ea19e67078
sam bacha <sam@manifoldfinance.com>
2022-06-13 21:25:57 -0700

    feat: op/arb gas price oracle addresses

---
+++ framework/src/gasprice_address/constants.ts
@@ -0,0 +1,12 @@
+/**
+* @export OVM_GASPRICE_ADDRESS
+* @summary Optimism GasPrice Oracle Address
+*/
+// ts
+export const OVM_GASPRICE_ADDRESS = "0x420000000000000000000000000000000000000F";
+
+/**
+* @export ARB_GASINFO_ADDRESS
+* @summary Arbitrium GasPrice Oracle Address
+*/
+export const ARB_GASINFO_ADDRESS = "0x000000000000000000000000000000000000006C";
